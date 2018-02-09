cc8c8fc393dcb081ecb207bd9f72b865c8ded923
gaelforget <gforget@mit.edu>
2018-02-08 20:43:37 -0500

    add parameter to select tracer subset

--- llc90drwn3_ptravrg.m
+++ llc90drwn3_ptravrg.m
@@ -1,7 +1,8 @@
-function []=llc90drwn3_ptravrg();
-%LLC90DRWN3_PTRAVRG();
-% read time series from diags/ subdirectory and write the corresponding
-% time average to diags_mean/ subdirectory
+function []=llc90drwn3_ptravrg(iList);
+%LLC90DRWN3_PTRAVRG(iList);
+%  read time series from diags/ subdirectory and write the corresponding
+%  time average to diags_mean/ subdirectory for tracers listed in
+%  iList ([1:106] by default).
 %
 %requirements:
 %  gcmfaces/       (matlab codes)
@@ -20,11 +21,15 @@ if isempty(mygrid);
    grid_load;
 end;
 
+if isempty(whos('iList'));
+  iList=[1:106];
+end;
+
 %% read time series, compute time mean, and write to disk
 
 dirIn='./';
 
-for iPtr=1:106;
+for iPtr=iList;
   fld=mean(rdmds([dirIn 'diags/ptr'],NaN,'rec',iPtr),4); 
   nmOut=sprintf('ptr%03d',iPtr); disp(nmOut);
   write2file([dirIn 'diags_mean/' nmOut '.bin'],fld);
