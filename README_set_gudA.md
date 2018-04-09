642de117c6a3942b5fb84f6f54fb297585f25938
gaelforget <gforget@mit.edu>
2018-04-09 11:48:22 -0400

    clean up README_set_gudA.md + further develop diags_set_gudA.m

--- README_set_gudA.md
+++ README_set_gudA.md
@@ -12,86 +12,93 @@ git clone https://github.com/darwinproject/tmp_code_devel
 git clone https://github.com/DrosteEffect/Colormaps-from-MatPlotLib2.0
 ```
 
-The `dirModel ` model output directory should contain the grid output and the `diags/` subdirectory. For the ECCO v4 model setup, grid output is available for download in netcdf format (e.g., [see here](http://gcmfaces.readthedocs.io/en/latest/prep_install.html)). The various `MITgcm` grids that have been used within `gcmfaces` are also available in binary format, e.g., via [this server](http://mit.ecco-group.org/opendap/ecco_for_las/version_4/grids/grids_output/).
+Hereafter, the `dirModel` directory is expected to contain the grid output and the `diags/` output subdirectory. For the ECCO v4 configuration, grid output is available for download in netcdf format (e.g., [see here](http://gcmfaces.readthedocs.io/en/latest/prep_install.html)). The various `MITgcm` grids that have been used within `gcmfaces` are additionally available for download in binary format, e.g., via [this server](http://mit.ecco-group.org/opendap/ecco_for_las/version_4/grids/grids_output/).
 
 ### 2) Interactively Analyze Output
 
-In the basic example below, we first load model output for Primary Production (units: `mmolC/m^3/s`) using `Matlab` (or `Octave`) and the `gcmfaces` toolbox. 
+Here, you will first load and display model output for Primary Production (units: `mmolC/m^3/s`) using the `gcmfaces` toolbox. To this end, launch `Matlab` (or `Octave`) and execute the following commands:
 
 ```
-%add software to path:
-p = genpath('gcmfaces/'); addpath(p);
-p = genpath('m_map/'); addpath(p);
+  %add software to path:
+  p = genpath('gcmfaces/'); addpath(p);
 
-%load data:
-grid_load;
-dirModel='./';
-dirDiags=[dirModel 'diags/'];
-fld=rdmds2gcmfaces([dirDiags 'gud_3d_set1*'],NaN,'rec',1);
+  %load data:
+  grid_load;
+  dirModel='./';
+  dirDiags=[dirModel 'diags/'];
+  fld=rdmds2gcmfaces([dirDiags 'gud_3d_set1*'],NaN,'rec',1);
 
-%Display gcmfaces object with 50 levels and 12 months:
-display(fld);
-
-```
-
-Then, we select a vertical level and depict time mean maps using `qwckplot.m` or the `m_map` toolbox.
+  %Display gcmfaces object structure:
+  display(fld);
 
 ```
-kk=1; m=mygrid.mskC(:,:,kk); 
-figureL; qwckplot(m.*mean(fld(:,:,kk,:),4));
-figureL; m_map_gcmfaces(fld,1.2,{'myCmap','inferno'}); 
+
+Then, select a vertical level and depict time mean maps using `qwckplot.m` or the `m_map` toolbox:
+
+```
+  %add software to path:
+  p = genpath('m_map/'); addpath(p);
+  p = genpath('Colormaps-from-MatPlotLib2.0/'); addpath(p);
+
+  %apply land mask and plot:
+  gcmfaces_global;
+  kk=1; m=mygrid.mskC(:,:,kk); 
+  figureL; qwckplot(m.*mean(fld(:,:,kk,:),4));
+  figureL; m_map_gcmfaces(fld,1.2,{'myCmap','inferno'}); 
 
 ```
 
 ### 3) The Standard Analysis Framework
 
-The `gcmfaces` standard analysis framework offers a slightly more advanced, systematic approach to model diagnostics ([documented here](http://gcmfaces.readthedocs.io/en/latest/)). For various diagnostic sets (e.g., `diags_set_gudA.m` below), `diags_driver.m` will process each monthly record, one at a time, and save results to a subdirectory within `dirMat` (e.g., 12 files will be created in the case of a climatology).
+The `gcmfaces` standard analysis framework offers a slightly more advanced, systematic approach to handle model diagnostics ([documented here](http://gcmfaces.readthedocs.io/en/latest/)). For pre-defined diagnostic sets (e.g., `diags_set_gudA.m` below), `diags_driver.m` can be called to process, e.g., monthly records, one at a time, and save results to a `dirMat` subdirectory (e.g., 12 files will be created in `mat/diags_set_gudA/` in the case below).
 
 
 ```
-    p = genpath('gcmfaces/'); addpath(p);
-    p = genpath('tmp_code_devel/'); addpath(p);
-    %
-    dirModel='./';
-    dirMat=[dirModel 'mat/'];
-    setDiags='gudA';
-    diags_driver(dirModel,dirMat,'climatology',setDiags);
+  %add software to path:
+  p = genpath('gcmfaces/'); addpath(p);
+  p = genpath('tmp_code_devel/'); addpath(p);
+    
+  %computational loop:
+  dirModel='./';
+  dirMat=[dirModel 'mat/'];
+  setDiags='gudA';
+  diags_driver(dirModel,dirMat,'climatology',setDiags);
 ```
 
-Once the computational phase has completed, results can be displayed using `diags_display.m` or using `diags_driver_tex.m`. The latter will use a `dirTex` directory to store plots and create a compilable `tex` file.
+Once the computational phase has completed, results can then be displayed using either `diags_display.m` or `diags_driver_tex.m`. The latter stores plots and create a compilable `tex` file in a `dirTex` directory (e.g., `tex/` in the case below).
 
 
 ```
-    p = genpath('gcmfaces/'); addpath(p);
-    p = genpath('m_map/'); addpath(p);
-    p = genpath('tmp_code_devel/'); addpath(p);
-    p = genpath('Colormaps-from-MatPlotLib2.0/'); addpath(p);
-    %
-    dirModel='./';
-    dirMat=[dirModel 'mat/']; setDiags='gudA';
-    dirTex=[dirModel 'tex/']; nameTex='gudA_plots';
-    diags_driver_tex(dirMat,{setDiags},dirTex,nameTex);
+  %add software to path:
+  p = genpath('m_map/'); addpath(p);
+  p = genpath('Colormaps-from-MatPlotLib2.0/'); addpath(p);
+    
+  %display loop:
+  dirModel='./';
+  dirMat=[dirModel 'mat/']; setDiags='gudA';
+  dirTex=[dirModel 'tex/']; nameTex='gudA_plots';
+  diags_driver_tex(dirMat,{setDiags},dirTex,nameTex);
 ```
 
 ### 4) Develop New Diagnostic Sets:
 
-In the following "XYZ" serves as a placeholder -- make sure to choose a more descriptive name for your new set of diagnostics. In a terminal window, one may then start as follows:
+In the following `XYZ` serves as a placeholder -- make sure to choose a more descriptive name for your new set of diagnostics. In a terminal window, start as follows:
 
 ```
 cp gcmfaces/gcmfaces_diags/diags_set_user.m tmp_code_devel/
 mv tmp_code_devel/diags_set_user.m tmp_code_devel/diags_set_XYZ.m
 ```
 
-Edit `diags_set_XYZ.m` to replace "user" with "XYZ" throughout. It can then be invoked via `diags_driver.m` and `diags_driver_tex.m` as explained above but with `setDiags` set to `'XYZ'`. The original exectution of `diags_driver.m` and `diags_driver_tex.m` will prompt you to edit `diags_set_XYZ.m` for each of four "userStep". Tip: look at `diags_set_gudA.m` for working examples. 
+Edit `diags_set_XYZ.m` to replace `user` with `XYZ` throughout. At this point, `diags_set_XYZ.m` can be invoked via `diags_driver.m` and `diags_driver_tex.m` as before but with `setDiags` set to `XYZ` instead of `gudA`. Along the way, user will be prompted to edit `diags_set_XYZ.m` for each of four `userStep` -- look at `diags_set_gudA.m` for working examples. Once `diags_set_XYZ.m` is at least partly implemented, uploading it to GitHub will make it available to collaborators (see next section).
 
 ### 5) Develop And Share Codes Via GitHub:
 
-Once your new set of diagnostics is at least partly implemented, uploading it to GitHub will make it available to collaborators. For example, again using "XYZ" as a placeholder, this can be done at the command line of a terminal window by typing:
+There are several advantages to using a system such as `GitHub` to archive, share, develop, and maintain codes. If you are new to `GitHub`, the following link may be a [a good place to start](https://guides.github.com/activities/hello-world/). To upload a new routine to GitHub, you may for example proceed as follows, at the command line of a terminal window:
 
 ```
 cd tmp_code_devel/
+git add diags_set_XYZ.m
 git commit diags_set_XYZ.m
 git push origin master
 ```
 
-There are several advantages to using a system such as `GitHub` to archive, share, develop, and maintain codes. To learn more about `GitHub`, this is a [a good place to start](https://guides.github.com/activities/hello-world/). 
--- diags_set_gudA.m
+++ diags_set_gudA.m
@@ -9,11 +9,15 @@ end;
 %%%%%%%%%%%%%%%%%%TEMPLATE%%%%%%%%%%%%%%%%%%%
 
 if strcmp(setDiags,'gudA')&userStep==1;%diags to be computed
-    listDiags='ppZm ppTop50m pp158W ppNorth ppSouth ppGlo';
+    listDiags='PP_Vi PP_Zm PP_158W PP_North PP_South PP_Glo';
+    listDiags=[listDiags ' Nfix_Vi Nfix_Zm Nfix_158W Nfix_North Nfix_South Nfix_Glo'];
+    listDiags=[listDiags ' Denit_Vi Denit_Zm Denit_158W Denit_North Denit_South Denit_Glo'];
+    listDiags=[listDiags ' PAR_Top PAR_Zm PAR_158W'];
+    listDiags=[listDiags ' PARF_Top PARF_Zm PARF_158W'];
 end;
 
 if strcmp(setDiags,'gudA')&userStep==2;%input files and variables
-    listFlds={'PP'};
+    listFlds={'PP','Nfix','Denit','PAR','PARF'};
     listFldsNames=deblank(listFlds);
     listFiles={'gud_3d_set1'};
     listSubdirs={[dirModel 'diags/']};
@@ -23,28 +27,32 @@ if strcmp(setDiags,'gudA')&userStep==3;%computational part;
     %preliminary steps:
     nl=length(mygrid.LATS);
     nr=length(mygrid.RC);
-    w50m=mk3D(mygrid.DRF,mygrid.mskC).*repmat(mygrid.RAC,[1 1 nr]);
-    w50m=w50m.*mygrid.hFacC; w50m(:,:,6:end)=NaN;
+    drf3d=mk3D(mygrid.DRF,mygrid.mskC).*mygrid.hFacC;
     volNorth=mk3D(mygrid.DRF,mygrid.mskC).*mygrid.hFacC.*mk3D(mygrid.RAC.*(mygrid.YC>0),mygrid.mskC);
     volSouth=mk3D(mygrid.DRF,mygrid.mskC).*mygrid.hFacC.*mk3D(mygrid.RAC.*(mygrid.YC<0),mygrid.mskC);
     volGlo=mk3D(mygrid.DRF,mygrid.mskC).*mygrid.hFacC.*mk3D(mygrid.RAC,mygrid.mskC);;
 
-    ppZm=zeros(nl,nr,1); ppTop50m=repmat(0*mygrid.Depth,[1 1 1]); pp158W=zeros(321,nr,1);
-    ppNorth=zeros(nr,1); ppSouth=zeros(nr,1); ppGlo=zeros(nr,1);
-    kk=1;
-    %
-    fld=eval(listFlds{kk});
-    ppZm(:,:,kk)=calc_zonmean_T(fld);
-    %
-    ppTop50m(:,:,kk)=nansum(w50m.*fld,3)./nansum(w50m,3);
-    %
-    [LO,LA,tmp1,X,Y]=gcmfaces_section([-158 -158],[-89 90],fld);
-    pp158W(:,:,kk)=tmp1;
-    %
-    ppNorth(:,kk)=nansum(volNorth.*fld,0)./nansum(volNorth,0);
-    ppSouth(:,kk)=nansum(volSouth.*fld,0)./nansum(volSouth,0);
-    ppGlo(:,kk)=nansum(volGlo.*fld,0)./nansum(volGlo,0);
-    %
+    for kk=1:length(listFlds);
+      fld=eval(listFlds{kk});
+      %
+      tmpZm=calc_zonmean_T(fld);
+      [LO,LA,tmp158W,X,Y]=gcmfaces_section([-158 -158],[-89 90],fld);
+      eval([listFlds{kk} '_Zm=tmpZm;']); eval([listFlds{kk} '_158W=tmp158W;']);
+      %
+      if kk<4;
+        tmpVi=nansum(drf3d.*fld,3);
+        tmpGlo=nansum(volGlo.*fld)./nansum(volGlo);
+        tmpNorth=nansum(volNorth.*fld)./nansum(volNorth);
+        tmpSouth=nansum(volSouth.*fld)./nansum(volSouth);
+        eval([listFlds{kk} '_Vi=tmpVi;']); eval([listFlds{kk} '_Glo=tmpGlo;']);
+        eval([listFlds{kk} '_South=tmpSouth;']); eval([listFlds{kk} '_North=tmpNorth;']);
+      end;
+      %
+      if kk>=4;
+        eval([listFlds{kk} '_Top=fld(:,:,1);']);
+      end;
+    end;
+
 end;
 
 if strcmp(setDiags,'gudA')&userStep==-1;%plotting
@@ -64,44 +72,114 @@ if strcmp(setDiags,'gudA')&userStep==-1;%plotting
     if ~(lYear>=2); lYear=1; myTimes=[1:length(myTimes)]; end;
     %determine the number of full years (nYears)
     nYears=floor(length(myTimes)/lYear);
+    %records that correspond to each season
+    nrec=1+diff(myparms.recInAve);
+    DJF=[[12:12:nrec] [1:12:nrec] [2:12:nrec]];
+    MMA=[[3:12:nrec] [4:12:nrec] [5:12:nrec]];
+    JJA=[[6:12:nrec] [7:12:nrec] [8:12:nrec]];
+    SON=[[9:12:nrec] [10:12:nrec] [11:12:nrec]];
 
-    if (sum(strcmp(choicePlot,'all'))|sum(strcmp(choicePlot,'ppTop50mSeason')));
-        if addToTex; write2tex(fileTex,1,'fill in appropriate section title here',2); end;
-        cc=[0 1e-5];
-        nrec=1+diff(myparms.recInAve);
-        for mm=1:12;
-            fld=mean(alldiag.ppTop50m(:,:,mm:12:nrec),3);
-            figureL; m_map_gcmfaces(fld,1.2,{'myCaxis',cc},{'myCmap','inferno'});
-            myCaption={'fill in appropriate figure caption here'};
-            if addToTex; write2tex(fileTex,2,myCaption,gcf); end;
+   if (sum(strcmp(choicePlot,'all'))|sum(strcmp(choicePlot,'monthlyGlo')));
+        if addToTex; write2tex(fileTex,1,'Monthly Time Series For Global And Hemisphere Mean',2); end;
+
+        for vv=1:3;
+          switch vv;
+          case 1; tit='Primary Production (in mmol C/m3/s); global and hemispheric means'; aa=[0 nrec 0 1e-7]; vvName='alldiag.PP';
+          case 2; tit='N Fixation (in mmol C/m3/s); global and hemispheric means'; aa=[0 nrec 0 2e-9]; vvName='alldiag.Nfix';
+          case 3; tit='Denitrification (in mmol C/m3/s); global and hemispheric means'; aa=[0 nrec 0 1e-9]; vvName='alldiag.Denit';
+          end;
+          tmpGlo=eval([vvName '_Glo']);
+          tmpNorth=eval([vvName '_North']);
+          tmpSouth=eval([vvName '_South']);
+          figureL; plot(tmpGlo,'k','LineWidth',2); hold on;
+          plot(tmpNorth,'b','LineWidth',2); plot(tmpSouth,'r','LineWidth',2);
+          axis(aa); grid on; legend('Global','North. Hem.','South. Hem.');
+          myCaption={tit}; if addToTex; write2tex(fileTex,2,myCaption,gcf); end;
         end;
     end;
 
-    if (sum(strcmp(choicePlot,'all'))|sum(strcmp(choicePlot,'pp158Wseason')));
-        if addToTex; write2tex(fileTex,1,'fill in appropriate section title here',2); end;
+    if (sum(strcmp(choicePlot,'all'))|sum(strcmp(choicePlot,'seasonMaps')));
+        if addToTex; write2tex(fileTex,1,'Seasonal Mean Maps',2); end;
+
+        for vv=1:5;
+          switch vv;
+          case 1; tit='Primary Production (in mmol C/m2/s); vertical integral'; cc=[0 1e-3]; fld=alldiag.PP_Vi;
+          case 2; tit='N Fixation (in mmol C/m2/s); vertical integral'; cc=[0 2e-5]; fld=alldiag.Nfix_Vi;
+          case 3; tit='Denitrification (in mmol C/m2/s); vertical integral'; cc=[0 5e-5]; fld=alldiag.Denit_Vi;
+          case 4; tit='PAR at top of layer (in uEin/m2/s); top level'; cc=[0 500]; fld=alldiag.PAR_Top;
+          case 5; tit='total PAR at layer center (in uEin/m2/s); top level'; cc=[0 800]; fld=alldiag.PARF_Top;
+          end;
+          for mm=1:4;
+            switch mm;
+            case 1; ssn=DJF; ssnTxt='Dec.-Jan.-Feb. average';
+            case 2; ssn=MMA; ssnTxt='Mar.-Apr.-May. average';
+            case 3; ssn=JJA; ssnTxt='Jun.-Jul.-Aug. average';
+            case 4; ssn=SON; ssnTxt='Sep.-Oct.-Nov. average';
+            end;
+
+            tmp=mean(fld(:,:,ssn),3);
+            figureL; m_map_gcmfaces(tmp,1.2,{'myCaxis',cc},{'myCmap','inferno'});
+            myCaption={[tit ' for the ' ssnTxt]};
+            if addToTex; write2tex(fileTex,2,myCaption,gcf); end;
+          end;
+        end;
+    end;
+
+    if (sum(strcmp(choicePlot,'all'))|sum(strcmp(choicePlot,'season158W')));
+        if addToTex; write2tex(fileTex,1,'Seasonal Mean 158W Sections',2); end;
         [LO,LA,tmp1,X,Y]=gcmfaces_section([-158 -158],[-89 90],mygrid.hFacC);
-        cc=[0 1e-5];
-        nrec=1+diff(myparms.recInAve);
-        for mm=1:12;
-            fld=mean(alldiag.pp158W(:,:,mm:12:nrec),3);
-            figureL; set(gcf,'Renderer','zbuffer'); pcolor(X,Y,fld);
+
+        for vv=1:5;
+          switch vv;
+          case 1; tit='Primary Production (in mmol C/m2/s); 158W section'; cc=[0 1e-5]; fld=alldiag.PP_158W;
+          case 2; tit='N Fixation (in mmol C/m2/s); 158W section'; cc=[0 5e-7]; fld=alldiag.Nfix_158W;
+          case 3; tit='Denitrification (in mmol C/m2/s); 158W section'; cc=[0 5e-7]; fld=alldiag.Denit_158W;
+          case 4; tit='PAR at top of layer (in uEin/m2/s); 158W section'; cc=[0 500]; fld=alldiag.PAR_158W;
+          case 5; tit='total PAR at layer center (in uEin/m2/s); 158W section'; cc=[0 800]; fld=alldiag.PARF_158W;
+          end;
+          for mm=1:4;
+            switch mm;
+            case 1; ssn=DJF; ssnTxt='Dec.-Jan.-Feb. average';
+            case 2; ssn=MMA; ssnTxt='Mar.-Apr.-May. average';
+            case 3; ssn=JJA; ssnTxt='Jun.-Jul.-Aug. average';
+            case 4; ssn=SON; ssnTxt='Sep.-Oct.-Nov. average';
+            end;
+
+            tmp=mean(fld(:,:,ssn),3);
+            figureL; set(gcf,'Renderer','zbuffer'); pcolor(X,Y,tmp);
             shading interp; axis([20 55 -300 0]); caxis(cc); colormap('inferno'); colorbar;
-            myCaption={'fill in appropriate figure caption here'};
+            myCaption={[tit ' for the ' ssnTxt]};
             if addToTex; write2tex(fileTex,2,myCaption,gcf); end;
+          end;
         end;
     end;
 
-    if (sum(strcmp(choicePlot,'all'))|sum(strcmp(choicePlot,'ppZmSeason')));
-        if addToTex; write2tex(fileTex,1,'fill in appropriate section title here',2); end;
+    if (sum(strcmp(choicePlot,'all'))|sum(strcmp(choicePlot,'seasonZM')));
+        if addToTex; write2tex(fileTex,1,'Seasonal Mean Zonal Mean',2); end;
         X=mygrid.LATS*ones(1,length(mygrid.RC)); Y=ones(length(mygrid.LATS),1)*(mygrid.RC');
-        cc=[0 1e-5];
-        nrec=1+diff(myparms.recInAve);
-        for mm=1:12;
-            fld=mean(alldiag.ppZm(:,:,mm:12:nrec),3);
-            figureL; set(gcf,'Renderer','zbuffer'); pcolor(X,Y,fld);
+
+        for vv=1:5;
+          switch vv;
+          case 1; tit='Primary Production (in mmol C/m2/s); zonal mean'; cc=[0 1e-5]; fld=alldiag.PP_Zm;
+          case 2; tit='N Fixation (in mmol C/m2/s); zonal mean'; cc=[0 2e-7]; fld=alldiag.Nfix_Zm;
+          case 3; tit='Denitrification (in mmol C/m2/s); zonal mean'; cc=[0 2e-8]; fld=alldiag.Denit_Zm;
+          case 4; tit='PAR at top of layer (in uEin/m2/s); zonal mean'; cc=[0 500]; fld=alldiag.PAR_Zm;
+          case 5; tit='total PAR at layer center (in uEin/m2/s); zonal mean'; cc=[0 800]; fld=alldiag.PARF_Zm;
+          end;
+          for mm=1:4;
+            switch mm;
+            case 1; ssn=DJF; ssnTxt='Dec.-Jan.-Feb. average';
+            case 2; ssn=MMA; ssnTxt='Mar.-Apr.-May. average';
+            case 3; ssn=JJA; ssnTxt='Jun.-Jul.-Aug. average';
+            case 4; ssn=SON; ssnTxt='Sep.-Oct.-Nov. average';
+            end;
+
+            tmp=mean(fld(:,:,ssn),3);
+            figureL; set(gcf,'Renderer','zbuffer'); pcolor(X,Y,tmp);
             shading interp; axis([-90 90 -300 0]); caxis(cc); colormap('inferno'); colorbar;
-            myCaption={['phyto-plankton -- log10[C] where C is the month nb ' num2str(mm)  ' mean, annual mean (in mgC/m3)']};
+            myCaption={[tit ' for the ' ssnTxt]};
             if addToTex; write2tex(fileTex,2,myCaption,gcf); end;
+          end;
         end;
     end;
 
