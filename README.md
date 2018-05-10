02135a7c4be42dd7ff6503689b3c70fec054b1f2
gaelforget <gforget@mit.edu>
2018-05-10 12:56:27 -0400

    add functions to analyze output at 155W + document the various functions in README.md

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# tmp_code_devel
+# tmp\_code\_devel
 a repository to share codes that are under development
+
+
+#### 155W section diagnostics
+
+- `read_slice_155W.m` is a Matlab / Octave function that reads in a variable once it has been interpolated to 155W using ` interp_to_155W.m`
+- ` interp_to_155W.m` interpolates from model grid to 155W.
+- `interp_to_155W_uv.m` interpolates velocity to 155W (see C-grid docs).
+
+#### standard diagnostics
+
+- `README_set_gudA.md` documents method to systematically depict model output, including via `diags_set_drwn3.m` and `diags_set_gudA.m`.
+- `diags_set_drwn3.m` is a standard set of biomass and plankton distribution diagnostics.
+- `diags_set_gudA.m` is a standard set of diagnostics with relevance to primary production.
+
+### basic diagnostics
+
+- `eccov4_climplot.m` generates maps and sections of the physical ocean state variables (MLD, T, S, U, V,...).
+- `llc90drwn3_ptravrg.m` time averages tracer model output.
+- `llc90drwn3_ptrplot.m` plots time-averaged tracer output.
+
---
+++ interp_to_155W.m
@@ -0,0 +1,51 @@
+
+%dirIn=[pwd '/diags_state/']; list0=dir([dirIn 'state_3d_set1*.data']);
+%dirIn=[pwd '/diags_ptr/']; list0=dir([dirIn 'ptr_3d_set1*.data']);
+dirIn=[pwd '/diags_gud/']; list0=dir([dirIn 'gud_3d_set1*.data']);
+
+if isempty(whos('MGtoIG'));
+p = genpath('gcmfaces/'); addpath(p);
+grid_load; gcmfaces_global;
+
+latlon = load('latlon_155W.mat');
+lat=latlon.latgrad;
+lon=latlon.longrad*ones(size(lat));
+
+interp=gcmfaces_interp_coeffs(lon(:),lat(:));
+MGtoIG.gridName='llc90';
+MGtoIG.lon=lon;
+MGtoIG.lat=lat;
+MGtoIG.matrix=interp.SPM;
+end;
+
+dirOut=[dirIn(1:end-1) '_155W/'];
+if ~isdir(dirOut); mkdir(dirOut); end;
+
+for ii=1:length(list0);
+disp(ii);
+%
+fld=read_bin([dirIn list0(ii).name]);
+nfld=size(fld{1},3)/50;
+fld=fld.*repmat(mygrid.mskC,[1 1 nfld]);
+%
+tmp1=convert2vector(fld);
+[tmp0,s3]=size(tmp1);
+tmp0=1*~isnan(tmp1);
+tmp1(isnan(tmp1))=0;
+%
+tmp0=MGtoIG.matrix*tmp0;
+tmp1=MGtoIG.matrix*tmp1;
+tmp1=reshape(tmp1./tmp0,[size(MGtoIG.lon) s3]);
+%
+sections=NaN*zeros(length(lon),50,nfld);
+for jj=1:nfld;
+  sections(:,:,jj)=tmp1(1,:,[1:50]+50*(jj-1));
+end;
+save([dirOut list0(ii).name(1:end-5) '.mat'],'sections');
+end;
+
+
+
+
+
+
